19ee990c1e5408e9a47a03fbc379f9bfeac3bedd
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-04-07 22:12:09 +0100

    LogIn and LogOut routes | Jwt usage refactor

--- customers/urls.py
+++ customers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import Register, Login
+from .views import Register, Login, CustomerView, LogOut
 
 urlpatterns = [
     path('register', Register.as_view()),
     path('login', Login.as_view()),
-
+    path('customer', CustomerView.as_view()),
+    path('logout', LogOut.as_view())
 ]
--- customers/views.py
+++ customers/views.py
@@ -4,7 +4,9 @@ from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.views import APIView
 from customers.serializers import CustomerSerializer, CreateCustomerSerializer
 from customers.models import Customer
+from books_rental.utils import get_jwt, decode_jwt
 import jwt, datetime
+
 
 
 
@@ -22,23 +24,12 @@ class Login(APIView):
     def post(self, request):
         username = request.data['username']
         password = request.data['password']
-
         user = Customer.objects.filter(username=username).first()
-
         if user is None:
             raise AuthenticationFailed('User not found')
         if not user.check_password(password):
             raise AuthenticationFailed('Incorrect login details')
-
-        
-        payload = {
-            'id' : user.id,
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=1000),
-            'iat': datetime.datetime.utcnow()
-        }
-
-        token = jwt.encode(payload, 'secret', algorithm='HS256').decode('utf-8')
-
+        token = get_jwt(user.id)
         response = Response()
         response.set_cookie(key='jwt', value=token, httponly=True)
         response.data = {'jwt': token}
@@ -46,7 +37,29 @@ class Login(APIView):
         return response
 
 
+class CustomerView(APIView):
+    def get(self, request):
+        token = request.COOKIES.get('jwt')
+        if not token:
+            raise AuthenticationFailed('Unauthenticated User!')
+        try:
+            payload = decode_jwt(token)
+        except jwt.ExpiredSignatureError:
+            raise AuthenticationFailed('Login Again')
+        user = Customer.objects.filter(id=payload['id']).first()
+        serializer = CustomerSerializer(user)
 
+        return Response(serializer.data)
+
+
+
+class LogOut(APIView):
+    def post(self, request):
+        response = Response()
+        response.delete_cookie('jwt')
+        response.data = {
+            "message": "User logged out"
+        }
 
 
 
