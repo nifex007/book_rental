b312bc97c7dbf587f8265ef31fe01b30415c8b30
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-04-12 00:54:02 +0100

    chore: removed staticfile storage setting

--- books_rental/settings.py
+++ books_rental/settings.py
@@ -150,7 +150,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
