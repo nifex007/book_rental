7879565db6c3026afbf1a4dc72f83b2486f4be26
Nifemi Sola-Ojo <nifex007@yahoo.com>
2021-04-11 17:46:09 +0100

    Update: computed new prices bases on book types and number of days

--- books_rental/pricing_policies.py
+++ books_rental/pricing_policies.py
@@ -1,8 +1,27 @@
+def regular(days):
+    if days<2:
+        return 2.0
+    else:
+        return float(2 + (days - 2) * 1.5)
+
+
+def novels(days):
+    if days<2:
+        return 2.0
+    elif days<3:
+        return 4.5
+    elif days>=3:
+        return float(1.5 * days)
+    
+
 def compute_charge(book_type, days):
-    charges_per_day = {
-        'F' : 3.0,
-        'R' : 1.5,
-        'N' : 1.5
+    
+    charges = {
+        'F' : float(3.0 * days),
+        'R' : regular(days),
+        'N' : novels(days)
     }
 
-    return float(charges_per_day[book_type] * days)
+    return charges[book_type]
+
+ 
